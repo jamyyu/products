8962845f2676b5a934cd67d416e10dc0be97660c
jamyyu <jamy11212@gmail.com>
2022-05-08 00:05:47 +0800

    refactor

--- products.py
+++ products.py
@@ -1,35 +1,58 @@
 import os
 
+
+
+
 #讀取檔案
-products = [ ]
-if os.path.isfile('products.csv'): #檢查檔案在不在
-    print('已搜尋到檔案')
-    with open('products.csv', 'r', encoding ='utf-8') as f:
+def read_file(filename):
+    products = [ ]
+    with open(filename, 'r', encoding ='utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue
             name,price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-else:
-    ptint('無法搜尋檔案')
+    return products
 
 #讓使用者輸入
-while True:
-    name = input('請輸入商品名稱：')
-    if name == 'q':
-         break
-    price = input('請輸入商品價格：')
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱：')
+        if name == 'q':
+             break
+        price = input('請輸入商品價格：')
+        products.append([name, price])
+    print(products)
+    return products
 
 #印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格為：', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格為：', p[1])
 
 #寫入檔案
-with open ('products.csv', 'w', encoding ='utf-8') as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n') #逗點區隔，存成csv才可以在excel打開有分格
-         
+def write_file(filename, products):
+    with open (filename, 'w', encoding ='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n') #逗點區隔，存成csv才可以在excel打開有分格
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #檢查檔案在不在
+        print('已搜尋到檔案')
+        products = read_file(filename)
+    else:
+        ptint('無法搜尋檔案')
+        
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+
+main()
+
+
+
